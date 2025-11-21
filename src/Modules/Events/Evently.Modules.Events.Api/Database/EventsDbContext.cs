656531ff81f75a4c6ba14ca83e3b7c65786f74ed
erfan <erfan.88.eslami@gmail.com>
2025-11-21 14:17:04 +0330

    feat: add create event api

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Database/EventsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Evently.Modules.Events.Api.Events;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Modules.Events.Api.Database;
+
+public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options)
+{
+    internal DbSet<Event>  Events { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Events);
+
+        modelBuilder.Entity<Event>()
+            .Property(x => x.Title).HasMaxLength(256);
+
+        modelBuilder.Entity<Event>()
+            .Property(x => x.Description).HasMaxLength(2048);
+        
+        modelBuilder.Entity<Event>()
+            .Property(x => x.Location).HasMaxLength(1024);
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Api/Events/CreateEvent.cs
@@ -0,0 +1,47 @@
+﻿using Evently.Modules.Events.Api.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Api.Events;
+
+public static class CreateEvent
+{
+    public static void MapEndPoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("events", async (Request request, EventsDbContext dbContext, CancellationToken cancellationToken) =>
+        {
+            var @event = new Event()
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Description = request.Description,
+                Location = request.Location,
+                StartsAtUtc = request.StartsAtUtc,
+                EndsAtUtc = request.EndsAtUtc,
+                Status = EventStatus.Draft
+            };
+
+            dbContext.Events.Add(@event);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return Results.Ok(@event.Id);
+
+        }).WithTags(Tags.Events);
+    }
+
+    private sealed class Request
+    {
+        public string Title{ get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime StartsAtUtc { get; set; }
+
+        public DateTime? EndsAtUtc { get; set; }
+    }
+}
